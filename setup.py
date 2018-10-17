f29c399a01239aef27cfc15893f9780461e47b49
K Lauer <klauer@slac.stanford.edu>
2018-10-17 11:15:06 -0700

    BLD: tweak requirements for usage on travis

--- setup.py
+++ setup.py
@@ -26,10 +26,12 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
+
 with open(path.join(here, 'requirements.txt')) as requirements_file:
     # Parse requirements.txt, ignoring any commented-out lines.
-    requirements = [line for line in requirements_file.read().splitlines()
-                    if not line.startswith('#')]
+    requirements = [line
+                    for line in requirements_file.read().splitlines()
+                    if not line.startswith('#') and not line.startswith('-e ')]
 
 
 setup(
