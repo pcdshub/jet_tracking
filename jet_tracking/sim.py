249d3a5e880a286ed11829503250c7a00c096aed
K Lauer <klauer@users.noreply.github.com>
2018-11-29 15:36:55 -0800

    Update jet_tracking/sim.py
    Co-Authored-By: teddyrendahl <trendahl@slac.stanford.edu>

--- jet_tracking/sim.py
+++ jet_tracking/sim.py
@@ -117,7 +117,7 @@ def generate_simulation(motor_precision=3,
 
     2. A ``SynAxis``, ``inj_x``, is instantiated. A simplification is made that
     the position of the jet is equivalent to the position of this motor. In
-    order to simulate a flucuating jet, ``jet_noise`` can be modified to
+    order to simulate a fluctuating jet, ``jet_noise`` can be modified to
     introduce Gaussian noise on top of the injector position when
     considering the jet position
 
