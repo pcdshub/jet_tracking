01dac110338e69cf2fc806d3205a00230a5795cd
amandashack <ajshack4@gmail.com>
2020-09-30 18:12:35 -0700

    mostly set up the layout except possibly more status items could be added

--- jet_tracking/testscreen.py
+++ jet_tracking/testscreen.py
@@ -3,8 +3,24 @@ from time import sleep
 from pydm import Display
 from qtpy.QtCore import QThread
 from os import path
-# from . import jet_control, jt_utils
 
+import os
+import json
+import cv2
+import numpy as np
+
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from qtpy import QtCore
+from pydm import Display
+from qtpy.QtWidgets import (QVBoxLayout, QHBoxLayout, QGroupBox,
+    QLabel, QLineEdit, QPushButton, QScrollArea, QFrame,
+    QApplication, QWidget, QGraphicsView, QGraphicsScene, QGraphicsRectItem, 
+    QGraphicsItem, QSizePolicy)
+
+from pydm.widgets import PyDMEmbeddedDisplay
+from pydm.utilities import connection
 
 class TrackThread(QThread):
 
@@ -116,19 +132,305 @@ class TrackThread(QThread):
             sleep(2)
 
 
-# class for jet tracking PyDM display
+class GraphicsView(QGraphicsView):
+
+    def __init__(self, parent=None):
+
+        super(GraphicsView, self).__init__(parent)
+
+        self.setMouseTracking(True)
+
+
+class GraphicsScene(QGraphicsScene):
+
+    def __init__(self, parent=None):
+
+        super(GraphicsScene, self).__init__(parent)
+
+class ComboBox(QComboBox):
+
+    def __init__(self, parent=None):
+
+        super(ComboBox, self).__init__(parent)
+
+class PushButton(QPushButton):
+
+    def __init__(self, parent=None):
+
+        super(PushButton, self).__init__(parent)
+
+class Label(QLabel):
+    def __init__(self, parent=None):
+
+        super(Label, self).__init__(parent)
+    
+    def setTitleStylesheet(self):
+        self.setStyleSheet("\
+                qproperty-alignment: AlignCenter;\
+                border: 1px solid #FF17365D;\
+                border-top-left-radius: 15px;\
+                border-top-right-radius: 15px;\
+                background-color: #FF17365D;\
+                padding: 5px 0px;\
+                color: rgb(255, 255, 255);\
+                max-height: 35px;\
+                font-size: 14px;\
+            ")
+    def setSubtitleStyleSheet(self):
+        self.setStyleSheet("\
+                qproperty-alignment: AlignCenter;\
+                border: 1px solid #FF17365D;\
+                background-color: #FF17365D;\
+                padding: 5px 0px;\
+                color: rgb(255, 255, 255);\
+                font-size: 12px;\
+            ")
+    def setCameraStylesheet(self):
+        #this will change based on the status given back
+        self.setStyleSheet("\
+                qproperty-alignment: AlignCenter;\
+                border: 1px solid #FF17365D;\
+                background-color: red;\
+                padding: 5px 0px;\
+                color: rgb(255, 255, 255);\
+                font-size: 12px;\
+            ")
+
+
+class JetTracking(Display):
+    def __init__(self, parent=None, args=None, macros=None):
+        super(JetTracking, self).__init__(parent=parent, args=args, macros=macros)
+
+        #reference to PyDMApplication - this line is what makes it so that you can avoid 
+        #having to define main() and instead pydm handles that for you - it is a subclass of QWidget
+        self.app = QApplication.instance()
+
+        #load data from file
+        self.load_data()
+
+        #assemble widgets
+        self.setup_ui()
+
+    def minimumSizeHint(self):
+        
+        return(QtCore.QSize(1000,600))
+    
+    def ui_filepath(self):
+
+        #no Ui file is being used as of now
+        return(None)
+    
+    def load_data(self):
+
+        #this is responsible for opening the database and adding the information to self.data
+        #https://slaclab.github.io/pydm-tutorial/action/python.html
+
+        pass
+    def setup_ui(self):
+        #set default style sheet
+        #self.setDefaultStyleSheet()
+        
+        #create layout
+        self._layout = QVBoxLayout()
+        self.setLayout(self._layout)
+
+        #give it a title
+        self.lbl_title = Label("Jet Tracking")
+        self.lbl_title.setTitleStylesheet()
+        self._layout.addWidget(self.lbl_title)
+        self._layout.addStretch(1)
+        self.lbl_title.setMaximumHeight(35)
+
+        #add a main layout for under the title which holds graphs and user controls
+        self.layout_main = QHBoxLayout()
+        self._layout.addLayout(self.layout_main)
+
+        #####################################################################
+        # make views/scenes to hold pydm graphs
+        #####################################################################
+        self.view_graph1 = GraphicsView()
+        self.view_graph2 = GraphicsView()
+        self.view_graph3 = GraphicsView()
+        self.scene_graph1 = GraphicsScene()
+        self.scene_graph2 = GraphicsScene()
+        self.scene_graph3 = GraphicsScene()
+        self.view_graph1.setScene(self.scene_graph1)
+        self.view_graph2.setScene(self.scene_graph2)
+        self.view_graph3.setScene(self.scene_graph3) 
+        
+        ######## setup layout ##########
+        self.frame_graph = QFrame()
+        self.frame_graph.setMinimumHeight(500)
+        self.layout_graph = QVBoxLayout()
+        self.frame_graph.setLayout(self.layout_graph)
+        self.layout_graph.addWidget(self.view_graph1)
+        self.layout_graph.addWidget(self.view_graph2)
+        self.layout_graph.addWidget(self.view_graph3)
+        ################################
+
+        #####################################################################
+        # set up user panel layout and give it a title
+        #####################################################################
+        self.frame_usr_cntrl = QFrame()
+        self.frame_usr_cntrl.setMinimumHeight(500)
+        self.layout_usr_cntrl = QVBoxLayout()
+        self.frame_usr_cntrl.setLayout(self.layout_usr_cntrl)
+
+        self.lbl_usr_cntrl = Label("User Controls")
+        self.lbl_usr_cntrl.setTitleStylesheet()
+        self.lbl_usr_cntrl.setMaximumHeight(35)
+        self.layout_usr_cntrl.addWidget(self.lbl_usr_cntrl)
+
+        #####################################################################
+        # make drop down menu for changing binning for sigma
+        #####################################################################
+
+        self.lbl_cbox_bins = Label("Sigma Bins")
+        self.lbl_cbox_bins.setSubtitleStyleSheet()
+        
+        self.cbox_bins = ComboBox()
+
+        #######setup layout#########
+        self.frame_cbox_bins = QFrame()
+        self.layout_cbox_bins = QHBoxLayout()
+        self.frame_cbox_bins.setLayout(self.layout_cbox_bins)
+        self.layout_usr_cntrl.addWidget(self.frame_cbox_bins)
+        self.layout_cbox_bins.addWidget(self.lbl_cbox_bins)
+        self.layout_cbox_bins.addWidget(self.cbox_bins)
+        ############################
+
+        ####################################################################
+        # make buttons to choose between devices for checking if we have beam
+        # currently either gas attenuator in the FEE or the Wave8
+        ####################################################################
+
+        self.lbl_init_initensity = Label("Initial beam Intensity RBV")
+        self.lbl_init_initensity.setSubtitleStyleSheet()
+        self.bttn_attenuator = PushButton("Gas Attenuator")
+        self.bttn_wave8 = PushButton("Wave8")
+
+        ####setup layout####
+        self.frame_init_initensity = QFrame()
+        self.layout_init_initensity = QHBoxLayout()
+        self.frame_init_initensity.setLayout(self.layout_init_initensity)
+        self.layout_usr_cntrl.addWidget(self.frame_init_initensity)
+        self.layout_init_initensity.addWidget(self.lbl_init_initensity)
+        self.layout_init_initensity.addWidget(self.bttn_attenuator)
+        self.layout_init_initensity.addWidget(self.bttn_wave8)
+        ####################
+
+        #####################################################################
+        # give a status area that displays values and current tracking
+        # reliability based on various readouts
+        #####################################################################
+        
+        self.lbl_status = Label("Status")
+        self.lbl_status.setTitleStylesheet()
+
+        self.lbl_camera = Label("Camera")
+        self.lbl_camera.setSubtitleStyleSheet()
+        self.lbl_camera_status = Label("No Tracing")
+        self.lbl_camera_status.setCameraStylesheet()
+        self.lbl_i0 = Label("Initial intensity (I0) RBV")
+        self.lbl_i0.setSubtitleStyleSheet()
+        self.lbl_i0_status = QLCDNumber(4)
+
+        self.lbl_gdet_i0 = Label("I/I0 at detector")
+        self.lbl_gdet_i0.setSubtitleStyleSheet()
+        self.lbl_gdet_i0_status = QLCDNumber(4)
+
+        ########setup layout###########
+        self.layout_usr_cntrl.addWidget(self.lbl_status)
+
+        self.frame_camera_status = QFrame()
+        self.frame_camera_status.setLayout(QHBoxLayout())
+        self.frame_camera_status.layout().addWidget(self.lbl_camera)
+        self.frame_camera_status.layout().addWidget(self.lbl_camera_status)
+
+        self.frame_i0 = QFrame()
+        self.frame_i0.setLayout(QHBoxLayout())
+        self.frame_i0.layout().addWidget(self.lbl_i0)
+        self.frame_i0.layout().addWidget(self.lbl_i0_status)
+
+        self.frame_gdet_i0 = QFrame()
+        self.frame_gdet_i0.setLayout(QHBoxLayout())
+        self.frame_gdet_i0.layout().addWidget(self.lbl_gdet_i0)
+        self.frame_gdet_i0.layout().addWidget(self.lbl_gdet_i0_status)
+
+        self.layout_usr_cntrl.addWidget(self.frame_camera_status)
+        self.layout_usr_cntrl.addWidget(self.frame_i0)
+        self.layout_usr_cntrl.addWidget(self.frame_gdet_i0)
+        ###############################
+
+        #########################################################################
+        # main buttons!!!!
+        #########################################################################
+
+        self.bttn_calibrate = QPushButton("Calibrate")
+        self.bttn_calibrate.setStyleSheet("\
+            background-color: yellow;\
+            font-size:12px;\
+            ")
+        self.bttn_start = QPushButton("Start")
+        self.bttn_start.setStyleSheet("\
+            background-color: green;\
+            font-size:12px;\
+            ")
+        self.bttn_stop = QPushButton("Stop")
+        self.bttn_stop.setStyleSheet("\
+            background-color: red;\
+            font-size:12px;\
+            ")
+
+        #########setup layout#########
+        self.frame_jjbttns = QFrame()
+        self.frame_jjbttns.setLayout(QHBoxLayout())
+        self.frame_jjbttns.layout().addWidget(self.bttn_calibrate)
+        self.frame_jjbttns.layout().addWidget(self.bttn_start)
+        self.frame_jjbttns.layout().addWidget(self.bttn_stop)
+
+        self.layout_usr_cntrl.addWidget(self.frame_jjbttns)
+        ##############################
+        
+        # add frame widgets to the main layout of the window
+        self.layout_main.addWidget(self.frame_graph)
+        self.layout_main.addWidget(self.frame_usr_cntrl)
+
+    
+    def setDefaultStyleSheet(self):
+
+        ### This should be done with a json file
+        
+        self.setStyleSheet("\
+            Label {\
+                qproperty-alignment: AlignCenter;\
+                border: 1px solid #FF17365D;\
+                border-top-left-radius: 15px;\
+                border-top-right-radius: 15px;\
+                background-color: #FF17365D;\
+                padding: 5px 0px;\
+                color: rgb(255, 255, 255);\
+                max-height: 35px;\
+                font-size: 14px;\
+            }")
+
+# Jason Reily please stop helping us
+# Arthur Brooks the Conservative heart
+
+
+'''
 class JetTrack(Display):
 
     def __init__(self, parent=None, args=None, macros=None):
 
         super(JetTrack, self).__init__(parent=parent,args=args, macros=macros)
-        '''def __init__(self, injector, camera, cspad, stopper, pulse_picker, wave8,
-                  params, macros, *args, **kwargs):'''
-        '''def __init__(self, jt_input, jt_output, jt_fake, macros, *args, **kwargs):
-        super().__init__(macros=macros, *args, **kwargs)'''
+        #def __init__(self, injector, camera, cspad, stopper, pulse_picker, wave8,
+        #          params, macros, *args, **kwargs):
+        #def __init__(self, jt_input, jt_output, jt_fake, macros, *args, **kwargs):
+        #super().__init__(macros=macros, *args, **kwargs)
 
-        '''self.track_thread = TrackThread(injector, camera, cspad, stoppper,
-                                        pulse_picker, wave8, params)'''
+        #self.track_thread = TrackThread(injector, camera, cspad, stoppper,
+        #                                pulse_picker, wave8, params)
         #self.track_thread = TrackThread(jt_input, jt_output, jt_fake)
 
         # connect GUI buttons to appropriate methods
@@ -151,7 +453,7 @@ class JetTrack(Display):
         self.ui.calibrate_btn.setEnabled(False)
 
         # call calibration method
-        '''jet_control.calibrate(injector, camera, cspad, params)'''
+        #jet_control.calibrate(injector, camera, cspad, params)
 
         self.ui.logger.write('Calibration complete - can now run jet tracking')
         self.ui.calibrate_btn.setEnabled(True)
@@ -173,3 +475,4 @@ class JetTrack(Display):
         self.ui.stop_btn.setEnabled(False)
         self.ui.start_btn.setEnabled(True)
         self.ui.calibrate_btn.setEnabled(True)
+'''
