c7f66550634ac60201ce9134939f081ae3a5fdcb
mossfr <moss.frank@gmail.com>
2021-08-11 17:04:43 -0700

    testing simulation number generator

--- jet_tracking/gui/widgets/editorWidgetUi.py
+++ jet_tracking/gui/widgets/editorWidgetUi.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QLabel, QSlider
-from gui.widgets.basicWidgets import QRangeSlider
+from gui.widgets.basicWidgets import QRangeSlider, Label, LineEdit, ComboBox
 
 class Editor_Ui(object):
 
@@ -18,4 +18,13 @@ class Editor_Ui(object):
         obj.layout_thresh.addWidget(obj.lbl_thresh)
         obj.layout_thresh.addWidget(obj.range_slider_thresh)
 
+        obj.lbl_percent_drop = QLabel("Dropped Shots (%)")
+        obj.box_percent_drop = LineEdit("10")
+        obj.box_percent_drop.valRange(0,100)
+
+        obj.layout_percent_drop = QHBoxLayout()
+        obj.layout_percent_drop.addWidget(obj.lbl_percent_drop, 75)
+        obj.layout_percent_drop.addWidget(obj.box_percent_drop)
+
         obj.layout.addLayout(obj.layout_thresh)
+        obj.layout.addLayout(obj.layout_percent_drop)
