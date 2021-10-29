0cc53c054ea704c837a3fa98dc8aadb5a682c470
Tyler Pennebaker <tntp@stanford.edu>
2021-10-29 13:01:44 -0700

    FIX: Remove old versioneer code from jet_tracking/__init__.py

--- jet_tracking/__init__.py
+++ jet_tracking/__init__.py
@@ -1,11 +1,3 @@
-from ._version import get_versions
-
-try:
-    __version__ = get_versions()['version']
-    del get_versions
-except Exception:
-    __version__ = '0.0.0-unknown'
-
 from . import _version
 
 __version__ = _version.get_versions()['version']
