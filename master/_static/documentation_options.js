01644c14609f7bdc17f65b6e8046b6f15ddeec02
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2022-02-08 00:37:05 +0000

    Update docs after building Travis build 158 of pcdshub/jet_tracking
    The docs were built from the tag 'master' against the commit
    6e0240e8fe3b216f1a040e93baea288f8d9e0f84.
    
    The Travis build that generated this commit is at
    https://app.travis-ci.com/pcdshub/jet_tracking/jobs/558715514.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.12/bin/doctr deploy --build-tags master --deploy-branch-name gh-pages --command '"doctr-versions-menu"'

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0.post123+g8b07e93',
+    VERSION: '0.1.0.post127+g6e0240e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
